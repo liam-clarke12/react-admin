e56786de1524c77898c6a755b7bd7e8d30fe6ef3
Liam <clarkeliam44@gmail.com>
2025-04-21 20:25:53 +0100

    zxc

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -2,13 +2,14 @@ import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
 const schema = a.schema({
   Todo: a.model({
-      content: a.string(),
-      isDone: a.boolean()
-    })
-    .authorization(allow => [allow.publicApiKey()])
+    content: a.string(),
+    isDone: a.boolean()
+  })
+  .authorization(allow => [allow.authenticated()])
 });
 
 export type Schema = ClientSchema<typeof schema>;
+
 export const data = defineData({
   schema
 });
