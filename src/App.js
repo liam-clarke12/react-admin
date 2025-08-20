af693171354fc46cc8f2018fd7e7676a7fff0677
Liam <clarkeliam44@gmail.com>
2025-08-20 23:24:38 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import { CssBaseline, ThemeProvider as MuiThemeProvider, CircularProgress, Box }
 
 import LandingPage from "./scenes/LandingPage";
 import ContactPage from "./scenes/ContactPage"; // ✅ matches src/scenes/ContactPage/index.jsx
-import FeaturesPage from "./scenes/FeaturesPage";
+import FeaturesPage from "./scenes/FeaturePage";
 import AccountPage from "./scenes/Account/Account";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
