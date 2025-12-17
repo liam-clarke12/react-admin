f56b921d01827cd08e1f0c26169b7345a2fcf1ab
Liam <clarkeliam44@gmail.com>
2025-12-17 21:43:18 +0000

    v2

--- src/scenes/HRP/Roles/index.jsx
+++ src/scenes/HRP/Roles/index.jsx
@@ -32,7 +32,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import { useAuth } from "../../../contexts/AuthContext";
 
 const API_BASE =
-  "https://z08auzr2ce.execute-api.eu-west-1.amazonaws.com/dev/api";
+  "https://z08auzr2ce.execute-api.eu-west-1.amazonaws.com/dev";
 
 function hashColor(str) {
   // deterministic pleasant-ish HSL from string
